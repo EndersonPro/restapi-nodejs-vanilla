30cf2d56d9eca9486d7d55d0af3f8041220e7960
Enderson Vizcaino <enjaviga@gmail.com>
2019-11-08 17:27:32 -0500

    Creando una forma de manejar las rutas

--- Server.js
+++ Server.js
@@ -5,25 +5,43 @@ module.exports = class Server {
   constructor(host, port) {
     this.port = port;
     this.host = host;
+    this.routeGet = [];
+    this.routePost = [];
   }
 
   initServer(cb) {
     this.server = http.createServer((req, res) => {
-      const reqUrl = url.parse(req.url, true);
-      if (reqUrl.pathname == '/' && req.method === 'GET') {
-        console.log(
-          'Request Type: ' + req.method + 'EndPoint ' + reqUrl.pathname
-        );
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.end(
-          JSON.stringify({
-            data: 'OK'
-          })
-        );
-      }
+      this.res = res;
+      this.req = req;
+      this.res.statusCode = 200;
+      this.res.setHeader('Content-Type', 'application/json');
+      let reqUrl = url.parse(req.url, true);
+
+      this.routeGet.forEach(({ path, callback }) => {
+        if (path == reqUrl.path) {
+          callback(req, res);
+        }
+      });
+
+      this.routePost.forEach(({ path, callback }) => {
+        if (path == reqUrl.path) {
+          callback(req, res);
+        }
+      });
     });
 
     this.server.listen(this.port, cb(this.port, this.host));
+  }
+
+  toJson(data) {
+    return JSON.stringify(data);
+  }
+
+  get(path, callback) {
+    this.routeGet = [...this.routeGet, { path, callback }];
+  }
+
+  post(path, callback) {
+    this.routePost = [...this.routePost, { path, callback }];
   }
 };
--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 const Server = require('./Server');
-
 const server = new Server('127.0.0.1', 9000);
+
+server.get('/', (req, res) => {
+  res.end(
+    JSON.stringify({
+      data: 'Funciona bro'
+    })
+  );
+});
+
+server.get('/otherRoute', (req, res) => {
+  res.end(
+    JSON.stringify({
+      data: 'Other Route'
+    })
+  );
+});
+
+server.post('/add', (req, res) => {
+  //   console.log(req.body);
+  const body = '';
+  req.on('data', function(data) {
+    body += data;
+    console.log(data);
+  });
+
+  //   const { msg } = JSON.parse(body);
+
+  //   console.log(msg);
+  console.log(body);
+  //   res.end({ msg });
+});
 
 server.initServer((port, host) =>
   console.log(`Servidor corriendo en el host ${host} puerto ${port}`)
 );
-
-// server.listen(9000, () => console.log('Servidor corriendo en el puerto 9000'));
